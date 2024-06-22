639a337cdcd05c0a451f5848f77acc8532b87ee7
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-06-22 02:08:59 -0300

    Conclusão da parte do Proprietário.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation ("com.loopj.android:android-async-http:1.4.9")
+    implementation ("com.loopj.android:android-async-http:1.4.11")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/estacionamento/Alterar.java
+++ app/src/main/java/com/example/estacionamento/Alterar.java
@@ -20,48 +20,46 @@ import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.ContentType;
 import cz.msebera.android.httpclient.entity.StringEntity;
 
-public class MainActivity extends AppCompatActivity {
-    EditText edtnome, edtcpf;
-    Button btncad, btnListar;
+public class Alterar extends AppCompatActivity {
+    EditText edtcodigo, edtnome, edtcpf;
+    Button btnalterar;
     AsyncHttpClient cliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        edtnome = (EditText) findViewById(R.id.edtnome);
-        edtcpf = (EditText) findViewById(R.id.edtcpf);
+        setContentView(R.layout.activity_alterar);
+        Bundle extras = getIntent().getExtras();
+        Proprietario p = new Proprietario();
+        Intent intent = getIntent();
+        p = (Proprietario) intent.getSerializableExtra("proprietario");
+        edtcodigo = (EditText) findViewById(R.id.edtid);
+        edtnome = (EditText) findViewById(R.id.edtnome2);
+        edtcpf = (EditText) findViewById(R.id.edtcpf2);
+        btnalterar = (Button) findViewById(R.id.button);
         cliente = new AsyncHttpClient();
-        //Botão de cadastrar
-        btncad = (Button) findViewById(R.id.button1);
-        btncad.setOnClickListener(new View.OnClickListener() {
+        edtcodigo.setText(String.valueOf(p.getId()));
+        edtnome.setText(String.valueOf(p.getNome()));
+        edtcpf.setText(String.valueOf(p.getCpf()));
+        btnalterar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (edtnome.getText().toString().isEmpty() || edtcpf.getText().toString().isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Existes campos em brancos!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Alterar.this, "Existes campos em brancos!", Toast.LENGTH_SHORT).show();
                 } else {
                     Proprietario obj = new Proprietario();
+                    obj.setId(Integer.parseInt(edtcodigo.getText().toString()));
                     obj.setNome(edtnome.getText().toString());
                     obj.setCpf(edtcpf.getText().toString());
-                    cadastrarProprietario(obj);
+                    alterarProprietario(obj);
                 }
             }
         });
-        //Botão de Listar os proprietários
-        btnListar = (Button) findViewById(R.id.button2);
-        btnListar.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, Lista_Proprietario.class);
-                startActivity(i);
-            }
-        });
-    }
+    } //Fora do onCreate
 
-    public void cadastrarProprietario(Proprietario obj) {
+    public void alterarProprietario(Proprietario obj) {
         String url;
-        url = "http://192.168.0.102:8081/proprietario";
-        // Crie um objeto JSON para os parâmetros
+        url = "http://192.168.0.102:8081/proprietario/" + obj.getId();
         JSONObject parametros = new JSONObject();
         try {
             parametros.put("nome", obj.getNome());
@@ -70,19 +68,19 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         StringEntity entity = new StringEntity(parametros.toString(), ContentType.APPLICATION_JSON);
-        cliente.post(MainActivity.this, url, entity, "application/json", new AsyncHttpResponseHandler() {
+        cliente.put(Alterar.this, url, entity, "application/json", new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 if (statusCode == 200) {
-                    Toast.makeText(MainActivity.this, "Proprietário cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
-                    edtnome.setText(null);
-                    edtcpf.setText(null);
+                    Toast.makeText(Alterar.this, "Proprietário alterado com sucesso!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Alterar.this, Lista_Proprietario.class);
+                    startActivity(i);
                 }
             }
 
             @Override
-            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
-                Toast.makeText(MainActivity.this, "Erro ao cadstrar proprietário: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(Alterar.this, "Erro ao alterar proprietário: " + error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
